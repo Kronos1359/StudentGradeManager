d8efd116b52e8d29177eaa3e4b79ab47facb789e
Kronos1359 <saachivarma135@gmail.com>
2026-07-05 09:13:34 +0530

    added diffeent functions forr all options and stuctured menu code, with input validation

--- main.py
+++ main.py
@@ -1,26 +1,48 @@
+def display_menu():
+    print("====Student Grade Manager====")
+    print("Menu:")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Search Student")
+    print("4. Delete Student")
+    print("5. Exit ")
 
+def add():
+    print("Not implemented")
+
+def view():
+    print("Not implemented")
+
+def search():
+    print("Not implemented")
+
+def delete():
+    print("Not implemented")
 
 def main():
     while True:
-        choice = int(input("===Student Grade Manager===\nEnter your choice:\n1. Add Student\n2. View Students\n3. Search Student\n4. Delete Student\n5. Exit "))
+        display_menu()
+
+        try:
+            choice = int(input("Enter your choice:"))
+            
+        except ValueError:
+            print("Pls enter only integer values.")
+            continue
 
         if choice == 1:
             add()
-            print("Not implemented")
         elif choice == 2:
             view()
-            print("Not implemented")
         elif choice == 3:
             search()
-            print("Not implemented")
         elif choice == 4:
             delete()
-            print("Not implemented")
         elif choice == 5:
             print("Bye")
             break
         else:
-            print("Invalid choice.")
+            print("Invalid choice: Pls enter integer numbers from 1 to 5.")
 
 if __name__ == "__main__":
     main()
